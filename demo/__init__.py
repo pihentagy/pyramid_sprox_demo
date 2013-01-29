c5c441681dfaf6e461c42ea039c48603b2703e3b
pihentagy <pihentagy@example.com>
2013-01-29 21:26:22 +0100

    save movie

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
 from sqlalchemy import engine_from_config
 
 import tw2.core
@@ -14,7 +15,8 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    config = Configurator(settings=settings)
+    session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
+    config = Configurator(settings=settings, session_factory=session_factory)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('movies_new', '/')
     config.scan()
@@ -23,8 +25,8 @@ def main(global_config, **settings):
     app = tw2.core.make_middleware(
         app,
         {
-            'toscawidgets.framework' : 'wsgi',
-            'toscawidgets.middleware.inject_resources' : True,
+            'toscawidgets.framework': 'wsgi',
+            'toscawidgets.middleware.inject_resources': True,
         },
         stack_registry=True,
     )
--- demo/views.py
+++ demo/views.py
@@ -1,4 +1,6 @@
-from pyramid.response import Response
+import datetime
+import transaction
+
 from pyramid.view import view_config
 
 from sprox.formbase import AddRecordForm
@@ -6,10 +8,14 @@ from sprox.formbase import AddRecordForm
 from .models import (
     DBSession,
     Movie,
+    Genre,
+    Director,
 )
+
 
 class NewMovieForm(AddRecordForm):
     __model__ = Movie
+
 
 @view_config(
     route_name='movies_new',
@@ -17,6 +23,18 @@ class NewMovieForm(AddRecordForm):
 )
 def new_movies_view(request):
     new_movie_form = NewMovieForm(DBSession)
+    values = dict(request.POST)
+    if request.method == 'POST''':
+        del values['sprox_id']
+        values['genre'] = DBSession.query(Genre).get(values['genre'])
+        values['directors'] = DBSession.query(Director).filter(
+            Director.id.in_(request.POST.getall('directors'))).all()
+        values['release_date'] = datetime.datetime.strptime(
+            values['release_date'], '%Y-%m-%d')
+        movie = Movie(**values)
+        with transaction.manager:
+            DBSession.add(movie)
+            request.session.flash("Movie " + movie.title + " inserted")
     return {
         'form': new_movie_form,
         'values': {},
